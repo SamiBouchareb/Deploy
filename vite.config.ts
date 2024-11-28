e4918f6a14e974514056ae125c8114106f348904
SamiBouchareb <Boucharebismail@gmail.com>
2024-11-28 21:11:20 +0100

    Fix: Remove path module and __dirname references

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Degos/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
   build: {
     outDir: 'dist',
     minify: 'terser',
     sourcemap: false,
     emptyOutDir: true,
     rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
       output: {
         manualChunks: undefined
       }
